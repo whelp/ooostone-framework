c8711ffb369c3b3eac730027eddb37288a4bf8c1
zhuyuan <zhuyuancabin@163.com>
2015-09-27 13:58:34 +0800

    提交

--- os-framework/src/main/java/com/ooostone/framework/exception/OSException.java
+++ os-framework/src/main/java/com/ooostone/framework/exception/OSException.java
@@ -1,5 +1,34 @@
 package com.ooostone.framework.exception;
 
-public class OSException extends Exception{
+public class OSException extends RuntimeException{
 
+	private static final long serialVersionUID = -1444059716246455677L;
+
+	public OSException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OSException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OSException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OSException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OSException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
 }
